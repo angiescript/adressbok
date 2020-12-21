a967d82fabaeddcc194826c96a4aec1e0998fbf6
angiescript <johansson.angelika@hotmail.com>
2020-12-21 15:41:44 +0100

    added comments and deleted unnecessary code.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    //Tankar in formulärets input-fält samt själva formuläret
     let add_button = document.getElementById("add_button");
     let add_name = document.getElementById("name");
     let add_number = document.getElementById("phone_no");
     let add_email = document.getElementById("email");
     let add_address = document.getElementById("address");
     let address_tbody = document.getElementById("tbody");
-    let delete_btn = document.getElementsByClassName("delete");
-
-
+   
+    //Om man klickar på "Lägg till" så läggs en ny rad till, därefter celler i den raden. 
+    //Därefter sätter vi cellens värde till vad användaren skrivt in i inputfälten. 
+    //Sist men inte minst lägger vi till en eventlyssnare för att hantera ta-bort-knapparna för de nya, tillagda personerna.
+    //Notera att dessa måste ligga EFTER att vi lagt in dem - om man lägger dem utanför denna eventlistener hittar den inte de nya personerna!
     add_button.addEventListener("click", () => {
         let newRow = document.createElement("tr");
         
@@ -41,7 +44,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
     });
-    
+
+    // Dessa rader krävs om man vill hårdkoda in några personer som ligger i adressboken från start!
     /* Array.from(delete_btn).forEach((delete_btn) => {
         delete_btn.addEventListener('click', () => {
             delete_btn.parentNode.parentNode.remove();
@@ -50,4 +54,4 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 
-}); //Dessa hör till DOM:en ta ej bort!
+}); 
